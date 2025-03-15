6b0c2746aa6f0c5c37613539851dc3667c16fba7
Fideliog <152099632+Fideliog@users.noreply.github.com>
2025-03-15 02:43:07 -0300

    Update app.js

--- Geren_Prods/app.js
+++ Geren_Prods/app.js
@@ -1,31 +1,29 @@
-import {produtos, gerarLinha, PrecosProd2, PrecosFilt, levaTudo, prodEstoque, PrecosProd, ProdTot } from './produtos.js';
+import {produtos, gerarLinha, precosProd2, precosFilt, levaTudo, prodEstoque, precosProd, prodTot, levaCada, nomeProd } from './produtos.js';
 
-console.log("PRODUTOS");
+console.log("Produtos");
 
 console.log(produtos);
 
 console.log(gerarLinha(60));
 
-console.log("PREÇOS"); 
+console.log("Preços"); 
 
-console.log(PrecosProd2);
-
-console.log(gerarLinha(60));
-
-console.log("ACIMA DE 10 REAIS:"); 
-
-console.log(PrecosFilt);
+console.log(precosProd2);
 
 console.log(gerarLinha(60));
 
-console.log("Total de produtos no estoque: ", ProdTot);
+console.log("Acima de 10 reais:"); 
+
+console.log(precosFilt);
 
 console.log(gerarLinha(60));
 
-console.log("Valor total dos produtos na loja: R$",levaTudo(PrecosProd, prodEstoque));
+console.log("Total de produtos no estoque: ", prodTot);
 
+console.log(gerarLinha(60));
 
+console.log("Preço total do estoque de cada produto:"); 
 
+console.log(levaCada(precosProd, prodEstoque, nomeProd));
 
-
-
+console.log("Valor total dos produtos na loja: R$",levaTudo(precosProd, prodEstoque));
